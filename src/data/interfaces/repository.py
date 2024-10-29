016941192928ad857c73a47803cfeeba90d009a6
Kaio Silva <kaiohp.silva1@gmail.com>
2024-10-29 17:04:41 -0300

    feat: add SqlRepository to interact with sql storage and management

---
+++ src/data/interfaces/repository.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+
+from src.domain import models
+
+
+class RepositoryInterface(ABC):
+    @abstractmethod
+    def add(self, batch: models.Batch):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get(self, reference) -> models.Batch:
+        raise NotImplementedError
---
+++ src/infra/database/repositories/repository.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm.session import Session
+
+from src.data.interfaces.repository import RepositoryInterface
+from src.domain import models
+
+
+class SqlRepository(RepositoryInterface):
+    def __init__(self, session: Session):
+        self.session = session
+
+    def add(self, batch: models.Batch):
+        self.session.add(batch)
+
+    def get(self, reference) -> models.Batch:
+        self.session.query(models.Batch).filter_by(reference=reference).one()
+
+    def list(self):
+        return self.session.query(models.Batch).all()
