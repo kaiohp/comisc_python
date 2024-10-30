bfd9da1d97dd10fc4372b291808eb4428dae52fe
Kaio Silva <kaiohp.silva1@gmail.com>
2024-10-30 16:58:52 -0300

    tests: add FakeRepository

---
+++ tests/infra/database/repositories/fake_repository.py
@@ -0,0 +1,18 @@
+from src.data.interfaces.repository import RepositoryInterface
+from src.domain.models import Batch
+
+
+class FakeRepository(RepositoryInterface):
+    def __init__(self, batches: list[Batch]) -> None:
+        self._batches = set(batches)
+
+    def add(self, batch: Batch):
+        self._batches.add(batch)
+
+    def get(self, reference: str):
+        return next(
+            batch for batch in self._batches if batch.reference == reference
+        )
+
+    def list(self):
+        return list(self._batches)
