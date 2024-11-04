9aaa732c6982ff660f49eb3ff4f5f001ea6d7d02
Kaio Silva <kaiohp.silva1@gmail.com>
2024-11-04 13:48:34 -0300

    tests: add postgres fixtures to make e2e tests

---
+++ tests/test_config.py
@@ -0,0 +1,88 @@
+import os
+
+import pytest
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import clear_mappers, sessionmaker
+
+from src.infra.database import orm
+from src.infra.database.config import Settings
+from src.infra.database.metadata import metadata
+
+
+@pytest.fixture
+def in_memory_db():
+    engine = create_engine('sqlite:///:memory:')
+    metadata.create_all(engine)
+
+    return engine
+
+
+@pytest.fixture
+def session(in_memory_db):
+    orm.start_mappers()
+    yield sessionmaker(bind=in_memory_db)()
+    clear_mappers()
+
+
+@pytest.fixture(scope='session')
+def postgres_db():
+    engine = create_engine(Settings().database_url)
+    metadata.create_all(engine)
+    return engine
+
+
+@pytest.fixture
+def postgres_session(postgres_db):
+    orm.start_mappers()
+    yield sessionmaker(bind=postgres_db)()
+    clear_mappers()
+
+
+@pytest.fixture
+def add_stock(postgres_session):
+    batches_added = set()
+    skus_added = set()
+
+    def _add_stock(lines):
+        for ref, sku, qty, eta in lines:
+            postgres_session.execute(
+                text(
+                    'INSERT INTO batches (reference, sku, _purchased_quantity, eta)'
+                    ' VALUES (:ref, :sku, :qty, :eta)'
+                ),
+                dict(ref=ref, sku=sku, qty=qty, eta=eta),
+            )
+            [[batch_id]] = postgres_session.execute(
+                text(
+                    'SELECT id FROM batches WHERE reference=:ref AND sku=:sku'
+                ),
+                dict(ref=ref, sku=sku),
+            )
+            batches_added.add(batch_id)
+            skus_added.add(sku)
+        postgres_session.commit()
+
+    yield _add_stock
+
+    for batch_id in batches_added:
+        postgres_session.execute(
+            text('DELETE FROM allocations WHERE batch_id=:batch_id'),
+            dict(batch_id=batch_id),
+        )
+        postgres_session.execute(
+            text('DELETE FROM batches WHERE id=:batch_id'),
+            dict(batch_id=batch_id),
+        )
+    for sku in skus_added:
+        postgres_session.execute(
+            text('DELETE FROM order_lines WHERE sku=:sku'),
+            dict(sku=sku),
+        )
+        postgres_session.commit()
+
+
+@pytest.fixture
+def get_api_url():
+    host = os.environ.get('API_HOST', 'localhost')
+    port = 8000 if host == 'localhost' else 80
+    return f'http://{host}:{port}'
