ec4fc954c8de7f5196e0382fcf3ee16a708a3f53
Kaio Silva <kaiohp.silva1@gmail.com>
2024-11-04 15:55:14 -0300

    refactor: rename main to server

--- src/main/server.py
+++ src/main/server.py
@@ -1,14 +1,11 @@
 from fastapi import FastAPI
 
-from src.infra.database import orm
 from src.main.routes import allocate
-
-orm.start_mappers()
 
 app = FastAPI()
 app.include_router(allocate.router)
 
 
-@app.get("/")
+@app.get('/')
 async def root():
-    return {"message": "Running"}
+    return {'message': 'Running'}
