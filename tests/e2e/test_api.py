60e821561b7b82eadeee07e25d25105bd5b2539c
Kaio Silva <kaiohp.silva1@gmail.com>
2024-11-04 13:51:49 -0300

    tests(test_api.py): add e2d test on allocation endpoint

--- tests/e2e/test_api.py
+++ tests/e2e/test_api.py
@@ -1,12 +1,30 @@
-import pytest
+import uuid
+from http import HTTPStatus
+
+import requests
 
 
-@pytest.mark.usefixtures('restart_api')
-def test_api_returns_allocation(add_stock):
+def random_suffix():
+    return uuid.uuid4().hex[:6]
+
+
+def random_sku(name='product'):
+    return f'sku-{name}-{random_suffix()}'
+
+
+def random_batch_ref(name='reference'):
+    return f'batch-{name}-{random_suffix()}'
+
+
+def random_order_ref(name='reference'):
+    return f'order-{name}-{random_suffix()}'
+
+
+def test_api_returns_allocation(add_stock, get_api_url):
     sku, othersku = random_sku(), random_sku('other')
-    earlybatch = random_batchref(1)
-    laterbatch = random_batchref(2)
-    otherbatch = random_batchref(3)
+    earlybatch = random_batch_ref('1')
+    laterbatch = random_batch_ref('2')
+    otherbatch = random_batch_ref('3')
 
     add_stock(
         [
@@ -17,9 +35,9 @@ def test_api_returns_allocation(add_stock):
     )
 
     data = {'order_reference': random_order_ref(), 'sku': sku, 'quantity': 3}
-    url = Settings().get_api_url()
+    url = get_api_url
 
-    r = requests.post(f'{url}/allocate', json=data)
+    r = requests.post(f'{url}/allocate', json=data, timeout=10)
 
-    assert r.status_code == 201
+    assert r.status_code == HTTPStatus.CREATED
     assert r.json()['batchref'] == earlybatch
