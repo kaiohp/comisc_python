bb27bd8e9dcaa38eb34a7b3f9d3fb0572fecee3f
Kaio Silva <kaiohp.silva1@gmail.com>
2024-11-04 13:45:29 -0300

    fix: remove foreing key appointment to a non primary key

--- src/infra/database/entities/batches.py
+++ src/infra/database/entities/batches.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Date, ForeignKey, Integer, String, Table
+from sqlalchemy import Column, Date, Integer, String, Table
 
 from src.infra.database.metadata import metadata
 
@@ -7,7 +7,7 @@ batches = Table(
     metadata,
     Column('id', Integer, primary_key=True, autoincrement=True),
     Column('reference', String(255)),
-    Column('sku', String(255), ForeignKey('products.sku')),
+    Column('sku', String(255)),
     Column('_purchased_quantity', Integer, nullable=False),
     Column('eta', Date, nullable=True),
 )
--- src/infra/database/entities/order_lines.py
+++ src/infra/database/entities/order_lines.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, Table
+from sqlalchemy import Column, Integer, String, Table
 
 from src.infra.database.metadata import metadata
 
@@ -6,7 +6,7 @@ order_lines = Table(
     'order_lines',
     metadata,
     Column('id', Integer, primary_key=True, autoincrement=True),
-    Column('sku', String(255), ForeignKey('products.sku')),
+    Column('sku', String(255)),
     Column('quantity', Integer, nullable=False),
     Column('order_reference', String(255)),
 )
