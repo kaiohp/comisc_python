f7a6f515dc70a8cd3431e557e16fc660ebdadd56
Kaio Silva <kaiohp.silva1@gmail.com>
2024-11-14 14:42:50 -0300

    refactor: removed model direct dependencie from allocation route

--- src/main/routes/allocate.py
+++ src/main/routes/allocate.py
@@ -4,7 +4,6 @@ from sqlalchemy.orm import sessionmaker
 
 from src.data import services
 from src.domain.errors import OutOfStock
-from src.domain.models import OrderLine
 from src.infra.database.config import Settings
 from src.infra.database.repositories.repository import SqlAlchemyRepository
 from src.main.models.order_line import OrderLine as OrderLineSchema
@@ -13,14 +12,15 @@ get_session = sessionmaker(bind=create_engine(Settings().database_url))
 router = APIRouter()
 
 
-@router.post('/allocate', status_code=status.HTTP_201_CREATED)
+@router.post('/allocate', status_code=status.HTTP_202_ACCEPTED)
 def allocate_endpoint(order_line: OrderLineSchema):
     session = get_session()
     repo = SqlAlchemyRepository(session)
-    line = OrderLine(**order_line.model_dump())
 
     try:
-        batch_reference = services.allocate(line, repo, session)
+        batch_reference = services.allocate(
+            **order_line.model_dump(), repo=repo, session=session
+        )
     except (OutOfStock, services.InvalidSku) as e:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail=str(e)
