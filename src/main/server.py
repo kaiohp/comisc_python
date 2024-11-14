3e6f64e103e79996eabdebec7d00a6de6cf88bac
Kaio Silva <kaiohp.silva1@gmail.com>
2024-11-14 14:43:25 -0300

    feat: insert deallocate and add_batch route to server

--- src/main/server.py
+++ src/main/server.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 
 from src.infra.database import orm
-from src.main.routes import allocate
+from src.main.routes import add_batch, allocate, deallocate
 
 orm.start_mappers()
 
 app = FastAPI()
 app.include_router(allocate.router)
+app.include_router(deallocate.router)
+app.include_router(add_batch.router)
 
 
 @app.get('/')
